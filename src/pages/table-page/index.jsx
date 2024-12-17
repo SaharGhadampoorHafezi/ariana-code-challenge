ad27b0d48fe47c630b88d808b9960ef5c6b99d92
Sahar Ghadampoor Hafezi <saharghadampoor@gmail.com>
2024-12-17 14:18:00 +0330

    implement the basic table, ui is not concluded

--- src/pages/table-page/index.jsx
+++ src/pages/table-page/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React from "react";
+import { userStore } from "../../store/store";
+import { calculateAge } from "../../utiles/age-calculator";
 
 export const TablePage = () => {
+  const users = userStore((state) => state.users);
+  console.log(users);
+
   return (
-    <div>
-      table
-    </div>
+    <form>
+      <tr>
+        <th>name</th>
+        <th>Skills set</th>
+        <th>Role</th>
+        <th>age</th>
+      </tr>
+      {users.map((user) => {
+        return (
+          <tr key={user.name}>
+            <td>{user.name}</td>
+            <td>{user.skillsSet}</td>
+            <td>{user.role}</td>
+            <td>{calculateAge(user.birthDate)}</td>
+          </tr>
+        );
+      })}
+    </form>
   );
-}
+};
