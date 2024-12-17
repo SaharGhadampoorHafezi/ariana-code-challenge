c79bbd6ad9f5c96a80dcabf7dbdc677c8c41b710
Sahar Ghadampoor Hafezi <saharghadampoor@gmail.com>
2024-12-17 14:17:04 +0330

    implement the basic form, ui is not concluded

--- src/modules/form/drop-down/drop-down.jsx
+++ src/modules/form/drop-down/drop-down.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 
-export const DropDown = () => {
+export const DropDown = ({ enteredValue, setEnteredValue }) => {
   const [open, setOpen] = useState(false);
-  const [select, setSelect] = useState("");
 
   const dropDownItems = ["react", "js", "next", "html", "css"];
 
@@ -12,14 +11,16 @@ export const DropDown = () => {
   return (
     <div className="relative">
       <button onClick={onClickHandler}>
-        {select ? select : "select you skills"}
+        {enteredValue.skillsSet ? enteredValue.skillsSet : "select you skills"}
       </button>
       {open ? (
         <ul className="absolute z-10">
           {dropDownItems.map((item) => {
             return (
               <li
-                onClick={() => setSelect(item)}
+                onClick={() =>
+                  setEnteredValue({ ...enteredValue, skillsSet: item })
+                }
                 key={item}
                 className=" bg-white"
               >
--- src/pages/form-page/index.jsx
+++ src/pages/form-page/index.jsx
@@ -1,11 +1,78 @@
-import React from 'react';
-import HeaderComponent from '../../components/header-component';
+import React, { useState } from "react";
+import HeaderComponent from "../../components/header-component";
+import { DropDown } from "../../modules/form";
+import { userStore } from "../../store/store";
 
 export const FormPage = () => {
+  const [enteredValue, setEnteredValue] = useState({
+    name: "",
+    role: "",
+    birthDate: "",
+    skillsSet: "",
+  });
+
+
+ 
+
+  const add = userStore((state) => state.addUser);
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    add(enteredValue);
+  };
+
   return (
-    <div className='flex flex-col align-center items-center'>
+    <div className="flex flex-col">
       <HeaderComponent />
-      
+      <div>
+        <h2 className="text-[main-black] font-semibold text-lg">
+          Personal info
+        </h2>
+        <p className="text-[second-gray] text-sm font-normal">
+          Update your photo and personal details here
+        </p>
+      </div>
+      <form onSubmit={onSubmitHandler}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            id="name"
+            value={enteredValue.name}
+            onChange={(e) =>
+              setEnteredValue({ ...enteredValue, name: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="role">Role</label>
+          <input
+            type="text"
+            id="role"
+            name={enteredValue.role}
+            onChange={(e) =>
+              setEnteredValue({ ...enteredValue, role: e.target.value })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="birth">Birth Date</label>
+          <input
+            type="date"
+            id="birth"
+            value={enteredValue.birthDate}
+            onChange={(e) => setEnteredValue({...enteredValue, birthDate: e.target.value})}
+          />
+        </div>
+        <div className="flex align-center items-center gap-2">
+          <label htmlFor="">skills set</label>
+          <DropDown
+            enteredValue={enteredValue}
+            setEnteredValue={setEnteredValue}
+          />
+        </div>
+        <button type="submit">click me</button>
+      </form>
     </div>
   );
-}
+};
