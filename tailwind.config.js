af7f613e28b5abd91f72d4158904ad5db28c88c7
Sahar Ghadampoor Hafezi <saharghadampoor@gmail.com>
2024-12-19 12:36:16 +0330

    add some color to the color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,22 @@
+// TODO: fix the colors that does not work
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        'dark-red': '#8A001B',
-        'main-black': '#181D27',
-        'second-gray': '#535862',
-      }
+        "dark-red": "#C60026",
+        "second-dark-red": "#8A001B",
+        "main-black": "#181D27",
+        "first-gray": "#414651",
+        "second-gray": "#535862",
+        "third-gray": "#D5D7DA",
+        "silver": "#E9EAEB",
+        "second-silver": "#868686",
+      },
     },
   },
   plugins: [],
-}
-
+};
