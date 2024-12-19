8227cc6051fe82654fe535c51edbee3adbf5db21
Sahar Ghadampoor Hafezi <saharghadampoor@gmail.com>
2024-12-19 12:37:15 +0330

    fix bug: fix the ui

--- src/modules/form/drop-down/drop-down.jsx
+++ src/modules/form/drop-down/drop-down.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { FiChevronDown } from "react-icons/fi";
 
 export const DropDown = ({ enteredValue, setEnteredValue }) => {
   const [open, setOpen] = useState(false);
@@ -10,19 +11,21 @@ export const DropDown = ({ enteredValue, setEnteredValue }) => {
   };
   return (
     <div className="relative">
-      <button onClick={onClickHandler}>
+      <button onClick={onClickHandler} className="w-[512px] h-[44px] rounded-lg border border-[third-gray] text-left pl-4 text-[#868686] flex  items-center gap-[350px]">
         {enteredValue.skillsSet ? enteredValue.skillsSet : "select you skills"}
+        <FiChevronDown size={10} />
       </button>
       {open ? (
-        <ul className="absolute z-10">
+        <ul className="absolute z-10 ">
           {dropDownItems.map((item) => {
             return (
               <li
+              className='border bg-white px-6 py-2 text-right'
                 onClick={() =>
                   setEnteredValue({ ...enteredValue, skillsSet: item })
                 }
                 key={item}
-                className=" bg-white"
+                
               >
                 {item}
               </li>
